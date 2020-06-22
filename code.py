b986d42d992b333e4ed908cb69ee0b0d3ea2fed4
jes22santos <jes22santos@gmail.com>
2020-06-22 10:23:02 +0100

    Code fixed

---
+++ code.py
@@ -0,0 +1,45 @@
+import os
+
+
+nextLineIsATimestamp = False
+newBlock = False
+blocks = []
+block = []
+formattedLine = None
+ts = None
+index = 0
+
+if os.path.exists("new_destination.csv"):
+  os.remove("new_destination.csv")
+else:
+  print("The file does not exist")
+
+writer = open("new_destination.csv", "a")
+
+with open('source.log', 'r') as reader:
+
+  for line in reader:
+    index +=1
+    if index == 13:
+      break
+    formattedLine = ' '.join(line.split())
+    formattedLine = formattedLine.replace(" ", ',')
+    
+    if '----' in line:
+      newBlock = True
+      nextLineIsATimestamp = True      
+    elif nextLineIsATimestamp:
+      nextLineIsATimestamp = False
+      # this is a new block, this is a timestamp
+      ts = formattedLine
+      # reset the state
+      newBlock = False
+    else:
+      block = formattedLine + "," + ts
+      writer.write(block)
+      writer.write("\n") 
+    
+    formattedLine = None
+
+
+writer.close()
